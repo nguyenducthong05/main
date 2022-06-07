7189f202b38e110354d7c49edd47e1d6cf71fba4
nguyenducthong05 <99624228+nguyenducthong05@users.noreply.github.com>
2022-06-07 10:33:40 +0700

    / Chi xinh xinh de thuong / Love You / sound / tick.mp3 @ nguyenducthong05

---
+++ Chi xinh xinh de thuong/Love You/config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo chị iu ! 🖐🖐🖐',
+    introDesc: `Em có điều này muốn hỏi chị nà, Chị nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Em biết Chị thích Em phải hông phải hông nà ? 😙',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là Chị thích Em đó nha :> ',
+    btnYes: 'Có, Chị thích Em nhiều nhiều lunn 💖💖💖',
+    btnNo: '💔 Em đang tự luyến hở ! 💔 ',
+    question:'Nói cho Em biết đi vì sao Chị thích Em nhiều nhiều zậy á 😊😊',
+    reasonPlaceholder: 'Bởi Vì',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì em đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Em biết mà, Em cũng thích chị lắm 🥰. Love you 300.000, thương thương nhiều nhìu 😘😘',
+    messDesc: '💬💬 Nhắn tin cho em nhớ. 💬💬',
+    btnAccept: 'Ò, chị biết gòi nà 💖',
+    messLink: 'https://www.facebook.com/Fanhammocuabo'
+}
